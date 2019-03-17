73955ca33a274e070e1ae097b88bc8e7f3131b75
Anum Asif <anum@cockar.com>
2019-03-18 00:11:07 +0300

    Add projects model to the project

--- rater/models.py
+++ rater/models.py
@@ -8,13 +8,13 @@ class Profile(models.Model):
     Add additional fields for django user
     '''
     user=models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    username=models.CharField(max_length=50)
-    profile_pic=ImageField(blank=True, manual_crop='800x800')
+    profile_pic=ImageField(blank=True, manual_crop='200x200')
     bio=HTMLField()
     contact_no=models.IntegerField(max_length=12)
 
+
     def __str__(self):
-        return self.username
+        return self.user.username
 
     def save_profile(self):
         self.save()
@@ -33,3 +33,33 @@ class Profile(models.Model):
     def filter_by_id(cls, id): 
         profile=Profile.objects.filter(user=id).first()
         return profile    
+
+class Project(models.Model):
+    '''
+    A class containing all the project information
+    '''
+    title=models.CharField(max_length=50)
+    image = ImageField(null=True, manual_crop='1280x720')
+    description = models.TextField(max_length=200)
+    link = models.URLField(null=True, blank=True, default='')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering=('-post_date')
+
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+
+    @classmethod
+    def get_all_projects(cls):
+        projects=Project.objects.all()
+        return projects
+
+    @classmethod
+    def get_project_by_userid(cls, id):
+        project=Project.objects.filter(user__pk=id)
+        
+             
