fe85543536c6c487bd694ebfb388d810ceed8f4a
Anum Asif <anum@cockar.com>
2019-03-19 04:43:07 +0300

    Write a function to get projects and render index page with it

--- rater/views.py
+++ rater/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from .models import Project
 # Create your views here.
+def index(request):
+    projects=Project.get_all_projects()
+    
+    return render(request, 'index.html', {"projects":projects})
+
